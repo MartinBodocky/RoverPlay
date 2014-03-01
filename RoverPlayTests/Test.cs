504d67a95a830b74d6ce4586211a8e13389a345a
MartinBodocky <unknown-user@192.168.0.2>
2014-03-01 15:18:46 +0000

    Initial check-in of module RoverPlayXamarin

---
+++ RoverPlayTests/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using RoverPlayXamarin;
+
+namespace RoverPlayTests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			var comp = new Computation ();
+			var result = comp.GetRandomNumber (12);
+			var expected ="12";
+			Assert.AreEqual (expected, result);
+		}
+	}
+}
+
---
+++ RoverPlayXamarin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RoverPlayXamarin
+{
+	public class Computation
+	{
+		public string GetRandomNumber(int lenght)
+		{
+			return "12";
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
